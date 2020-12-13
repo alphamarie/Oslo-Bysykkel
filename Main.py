55c4ca646c97e6616bb88624e65091c5e4498bf8
Alpha Marie <75942826+alphamarie@users.noreply.github.com>
2020-12-13 17:46:43 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,1 @@
-import json
-import requests
 
-class Hovedprogram:
-    main()
-
-    def __main__():
-        response = requests.get(" https://gbfs.urbansharing.com/oslobysykkel.no/station_information.json")
-        print(response.status_code)
